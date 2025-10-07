9bd9ab7796840ed3739dcda103bf9eb6913ce2e0
zhy <2959616852@qq.com>
2025-10-07 10:54:04 +0800

    run baseline

--- scripts/train_baseline.py
+++ scripts/train_baseline.py
@@ -2,7 +2,7 @@
 from ultralytics import YOLO
 
 # 1. 加载 YOLOv8l 预训练模型
-model = YOLO("yolov8l.pt")
+model = YOLO("yolov8n.pt") #数据集小的情况下l并不理想
 
 # 2. 直接调用 model.train() 进行训练
 model.train(
@@ -10,7 +10,7 @@ model.train(
     epochs=80,
     imgsz=1024,
     batch=4,
-    patience=20,
+    patience=10,
     hsv_v=0.2,
     degrees=5.0,
     scale=0.1,
